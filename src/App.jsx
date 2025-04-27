2df9a14befe5aea2fa7a8518ea767483b6b50e9a
hedir-ben-mooussa <hedirbenmoussa71@gmail.com>
2025-04-27 23:38:52 +0100

    ➕ updated assets + main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import { Footer } from './components/footer/footer'
 import { Header } from './components/header/header'
 import { StoreLocation } from './components/location/location'
+import { WelcomeSection } from './components/welcome-section/welcome'
+import { ProductList } from './components/product-list/productList';
+import { RedmiProducts } from './components/redmiProduct/redmiProduct';
+import { VideoList } from './components/video-section/video';
+import { XiaomiProducts } from './components/location/xiaomiProducts/xiaomiProducts';
 
 function App() {
   
@@ -9,6 +15,11 @@ function App() {
   return (
     <>
     <Header></Header>
+    <WelcomeSection></WelcomeSection>
+    <ProductList></ProductList>
+    <RedmiProducts></RedmiProducts>
+    <XiaomiProducts></XiaomiProducts>
+    <VideoList></VideoList>
     <StoreLocation></StoreLocation>
     <Footer></Footer>
     </>
