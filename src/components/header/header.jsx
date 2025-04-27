7fa459cc2d0aa2756d9b2015e9f3deb22c8e3138
hedir-ben-mooussa <hedirbenmoussa71@gmail.com>
2025-04-27 18:19:52 +0100

    ✅ added header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,26 +1,40 @@
 import './header.css'
+import { FaSearch } from "react-icons/fa";
+import { CiUser } from "react-icons/ci";
+import { IoCartOutline } from "react-icons/io5";
+import xiomi_logo from '../../assets/Xiaomi_logo.png'
 
-export function Header(){
-  const navbarList=["Boutique", "Smartphones", "Tablettes", "Smaet Home","Life Style", "Promotions","Nouvauté"] 
+export function Header() {
+    const navbarList = ["Boutique", "Smartphones", "Tablettes", "Smart Home", "Life Style", "Promotions", "Nouvauté"]
 
     return (
-        <div className='flex'>
-        <img src='' alt='logo' />
-        
-         <ul className="flex">
-         {navbarList.map((nav,index,list)=>{
-            
-            return(<li>{nav}</li>)
-         })}
-         </ul>
-         <div>
-            <div className=''>
-                <input type="search" placeholder='Rechercher ..'/>
-                <div>
-                    
+        <div className='flex space-between align-item-center bg-header '>
+            <img src={xiomi_logo} alt='logo' className='logo' style={{ paddingLeft: "20px" }} />
+            <ul className='flex'>
+                {navbarList.map((nav) => {
+
+                    return (<li >{nav}</li>)
+                })}
+            </ul>
+
+            <div className='flex align-item-center ' style={{ paddingRight: "20px" }}>
+                <div className='flex flex-col'>
+                    <input type="search" placeholder='Rechercher ..' className='search-input' />
+                    <div className='flex search-bar' >
+                        <FaSearch style={{ width: "20px", height: "20px", padding: "2px", paddingLeft: "13px" }} />
+                    </div>
                 </div>
+                <div className='relative '>
+                    <CiUser style={{ width: "30px", height: "30px" }} className='cursor-pointer' />
+                    <div style={{ position: "relative", display: "inline-block" }} className='cursor-pointer'>
+                        <IoCartOutline style={{ width: "30px", height: "30px" }} />
+                        <div className="notif">
+                            <span style={{ color: "white", fontSize: "10px" }}>0</span>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-         </div>
 
         </div>
     )
